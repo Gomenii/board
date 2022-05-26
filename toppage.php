fd26f6b5d873b6ee5239f6059e8b260f0f017865
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-05-26 14:56:03 +0900

    Add: toppage.php

---
+++ toppage.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+require_once('db_board.php');
+require_once('fanctions.php');
+// echo var_dump($_);
+// echo var_dump($_POST);
+// echo var_dump($_SERVER);
+// echo var_dump($_SESSION);
+// echo var_dump($_COOKIE);
+
+// echo var_dump($_POST['name']);
+// echo var_dump($_POST['pass']);
+// echo var_dump($_SESSION['name']);
+// echo var_dump($_SESSION['pass']);
+// echo var_dump($_SERVER['HTTP_HOST']);
+// echo var_dump($_SERVER['HTTP_REFERER']);
+// echo var_dump(strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']));
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="sample text">
+    <link rel="stylesheet" type="text/css" href="board.css">
+    <title>掲示板</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1><a href="toppage.php">サンプル掲示板</a></h1>
+    </div>
+
+    <div class="main">
+        <h2>【掲示板概要】この掲示板はポートフォリオ用となっています</h2>
+        <p>※投稿したりスレッドを作成するには、<a href="account_reg.php">新規登録</a>または<a href="login.php">ログイン</a>が必要です。</p>
+    </div>
+
+    <div class="main">
+        <a href="new_thread.php">
+            <button class="new_thread_btn" type="button">スレッド作成</button>
+        </a>
+    </div>
+
+    <div class="main">
+        <a href="thread1.php">
+            <button class="thread_btn" type="button">スレッド1</button>
+        </a>
+    </div>
+    
+    <div class="footer">
+
+    </div>
+</body>
+
+</html>
