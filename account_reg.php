b8ad77afb41a4eab9392fd65c7fb02553823f82d
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-05-28 00:15:21 +0900

    Add: アカウント登録エラー処理

--- account_reg.php
+++ account_reg.php
@@ -19,16 +19,55 @@ require_once('fanctions.php');
 
 <?php
 
-// アカウント条件
-// ユーザー名：16文字以内。英数字、記号のみ。被っていない。' '(空文字), 0, false, NULL以外。
-// パスワード：6文字以上。英数字のみ。' '(空文字)以外。
+// アカウント条件　 
+// ユーザー名：4～16文字以内。半角 英大文字・英小文字・数字・_ 。被っていない。' '(空文字),false, NULL以外。
+// パスワード：6～16文字以内。半角 英大文字・英小文字・数字・下記記号。' '(空文字)以外。
+// パスワードに使用できる記号： ! " # $ % & ' ( ) * + - , . / : ; < = > ? @ [ ] ^ _  { | } ~　
 // $_POSTが空（' '空文字, 0, false, NULL）ではなければ確認画面へ遷移
-if (isset($_POST['name']) && isset($_POST['pass'])) {
-    $_SESSION['name'] = $_POST['name'];
-    $_SESSION['pass'] = $_POST['pass'];
-    header('Location: account_reg_cfm.php');
-    exit();
+
+$postName = $_POST['name'];
+$postPass = $_POST['pass'];
+$postInput = !empty($postPass) && !empty($postPass);
+
+if (empty($postName) && empty($postPass)) {
+    $errors[] = '新規登録するユーザー名とパスワードを入力してください。<br>
+              （ユーザー名 4～16文字以内、パスワード 6～16文字以内）';
 }
+
+if (empty($postName) && !empty($postPass)) {
+    $errors[] = '※ユーザー名が入力されていません。';
+}
+
+if (!empty($postName) && empty($postPass)) {
+    $errors[] = '※パスワードが入力されていません。';
+}
+
+$maximum = 16;
+$nameMinimum = 4;
+$passMinimum = 6;
+$nameLength = strlen($postName);
+$passLength = strlen($postPass);
+
+if ($postInput) {
+    if ($nameLength < $nameMinimum || $nameLength > $maximum) {
+        $errors[] = '※ユーザー名が4～16文字ではありません。';
+    }
+    if ($passLength < $passMinimum || $passLength > $maximum) {
+        $errors[] = '※パスワードが6～16文字ではありません。';
+        echo $passLength;
+        echo $nameLength;
+    }
+}
+
+
+
+
+// if (isset($_POST['name']) && isset($_POST['pass'])) {
+//     $_SESSION['name'] = $_POST['name'];
+//     $_SESSION['pass'] = $_POST['pass'];
+//     header('Location: account_reg_cfm.php');
+//     exit();
+// }
 
 
 // // $_POSTが空（' '空文字, 0, false, NULL）ではなければ実行
@@ -71,11 +110,15 @@ if (isset($_POST['name']) && isset($_POST['pass'])) {
 
     <div class="main">
         <h2>アカウント新規登録</h2>
-        <p>ユーザー名とパスワード（6文字以上）を入力してください。</p>
     </div>
 
     <div class="main">
+
         <form action="" method="POST" class="account_reg">
+            <h4><?php foreach ($errors as $error) {
+                    echo $error;
+                    echo "\n";
+                } ?></h4>
             <p>ユーザー名　<input type="text" name="name" value="<?php if (isset($_POST['name'])) {
                                                                 echo htmlspecialchars($_POST['name'], ENT_QUOTES);
                                                             } ?>"></p>
@@ -84,12 +127,18 @@ if (isset($_POST['name']) && isset($_POST['pass'])) {
                                                             } ?>"></p>
             <p><input type="submit" name="account_reg_cfm_btn" value="確認画面にすすむ"></p>
         </form>
+
+        <p>※ユーザー名とパスワードは、同じものを使用しないでください。また、第三者に推測されやすいものを設定しないでください。</p>
+        <p>ユーザー名に使える文字列 : 半角英大文字、半角英小文字、半角数字、半角 _（アンダーバーのみ）</p>
+        <p>パスワードに使える文字列 : 半角英大文字、半角英小文字、半角数字、下記半角記号30種類</p>
+        <p>! " # $ % & ' ( ) * + - , . / : ; <=> ? @ [ ] ^ _ { | } ~</p>
+
     </div>
 
     <div class="main">
         <!-- 前のページが存在している & 前のページのアドレスにサイトのホスト名が含まれていれば、前のページに戻るボタンを表示する -->
-        <?php $host_name = $_SERVER['HTTP_HOST'];
-        if (!empty($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], $host_name) !== false) : ?>
+        <?php $hostName = $_SERVER['HTTP_HOST'];
+        if (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], $hostName) !== false) : ?>
             <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">
                 <button class="back_btn" type="button">前の画面に戻る</button>
             </a>
