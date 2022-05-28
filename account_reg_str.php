1a8818adf03a34964d938517a85c9f5e3195d089
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-05-28 23:00:35 +0900

    Add: first commit

---
+++ account_reg_str.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="sample text">
+    <link rel="stylesheet" type="text/css" href="board.css">
+    <title>アカウント新規登録に使える文字</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1><a href="toppage.php">サンプル掲示板</a></h1>
+    </div>
+
+    <div class="main">
+        <h2>アカウント新規登録に使える文字</h2>
+    </div>
+
+    <div class="main">
+
+        <div class="account_reg_str">
+            <h4>ユーザー名に使える文字</h4>
+            <ul>
+                <li>半角英大文字（A ~ Z）</li>
+                <li>半角英小文字（a ~ z）</li>
+                <li>半角数字（0 ~ 9）</li>
+                <li>半角アンダーバー（ _ ）</li>
+            </ul>
+            <p></p>
+            <p>※他のユーザーと同じ名前は使用できません。</p>
+            <p>※先頭に _（アンダーバー）は使用できません。</p>
+            <p>※空白（スペース）は使用できません。</p>
+        </div>
+
+        <div class="account_reg_str">
+            <h4>パスワードに使える文字</h4>
+            <ul>
+                <li>半角英大文字（A ~ Z）</li>
+                <li>半角英小文字（a ~ z）</li>
+                <li>半角数字（0 ~ 9）</li>
+                <li>半角記号 右記32種類 ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ￥ ] ^ _ ` { | } ~</li>
+            </ul>
+            <p>※先頭に記号は使用できません。</p>
+            <p>※空白（スペース）は使用できません。</p>
+        </div>
+
+    </div>
+
+    <div class="main">
+            <a href="account_reg.php">
+                <button class="back_btn" type="button">前の画面に戻る</button>
+            </a>
+    </div>
+
+    <div class="footer">
+
+    </div>
+</body>
+
+</html>
