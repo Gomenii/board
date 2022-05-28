bf68b0cfc40f2adb3c3da0715fa33d2d39655140
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-05-28 23:02:54 +0900

    Add: first commit

---
+++ error.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="sample text">
+    <link rel="stylesheet" type="text/css" href="board.css">
+    <title>エラー画面</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1><a href="toppage.php">サンプル掲示板</a></h1>
+    </div>
+
+    <div class="main">
+        <h2>エラー画面</h2>
+
+        <div class="error">
+            <p>エラーが発生しました。</p>
+            <p>予期せぬページからのアクセスが考えられます。</p>
+            <p>トップページから再度アクセスください。</p>
+        </div>
+
+        <a href="toppage.php">
+            <button class="back_btn" type="button">トップページに戻る</button>
+        </a>
+
+    </div>
+
+    <div class="footer">
+
+    </div>
+</body>
+
+</html>
