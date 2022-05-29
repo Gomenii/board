da7ab2a44e0f856a672531c8493d8fe5e24b1100
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-05-29 14:08:52 +0900

    Add: htmlspecialchars自作関数作成

--- fanctions.php
+++ fanctions.php
@@ -1,12 +1,7 @@
 <?php
-// function htmlsc($a) {
-//     if (isset($a)) {
-//         echo htmlspecialchars($a, ENT_QUOTES);
-//     }
-// }
 
-function a($a, $b) {
-    echo $a + $b;
-}
+function htmlsc($str) {
+        echo htmlspecialchars($str, ENT_QUOTES);
+    }
 
 ?>
