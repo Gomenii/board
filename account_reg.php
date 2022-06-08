4d9c9bbe7473ec3de1a39c752c1b296c3c93cbbb
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-06-08 12:45:52 +0900

    Improve: エラー処理

--- account_reg.php
+++ account_reg.php
@@ -15,109 +15,70 @@ require_once('fanctions.php');
 // echo var_dump($_SERVER['HTTP_HOST']);
 // echo var_dump($_SERVER['HTTP_REFERER']);
 // echo var_dump(strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']));
-?>
 
-<?php
 
-// アカウント条件　 
-// ユーザー名：4～16文字以内。半角 英大文字・英小文字・数字・_ 。被っていない。空文字,false,NULL以外。
-// パスワード：6～16文字以内。半角 英大文字・英小文字・数字・下記記号。'空文字以外。
+// 新規アカウント条件　 
+// ユーザー名：4～16文字以内。半角 英大文字・英小文字・数字・アンダーバー。　空文字,false,NULL以外。被っていない。
+// パスワード：6～16文字以内。半角 英大文字・英小文字・数字・下記記号。　　　空文字以外。
 // パスワードに使用できる記号32種類　! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ￥ ] ^ _ ` { | } ~　
 
-// ユーザー名とパスワードの両方が入力されていない場合（入力エラー）
-if (empty($_POST['name']) && empty($_POST['pass'])) {
-    $errors[] = '新規登録するユーザー名とパスワードを入力してください。<br>
-              （ユーザー名 4～16文字以内、パスワード 6～16文字以内）';
-} else {
+$postMaximum = 16;
+
+// ユーザー名の入力があれば変数に格納（未入力での送信はfalseとしたい為、!empty使用）
+if (!empty($_POST['name'])) {
     $postName = $_POST['name'];
-    $postPass = $_POST['pass'];
-    $postMaximum = 16;
     $nameMinimum = 4;
-    $passMinimum = 6;
     $nameLength = strlen($postName);
+}
+
+// パスワードの入力があれば変数に格納（未入力での送信はfalseとしたい為、!empty使用）
+if (!empty($_POST['pass'])) {
+    $postPass = $_POST['pass'];
+    $passMinimum = 6;
     $passLength = strlen($postPass);
 }
 
-// ユーザー名に入力がなく、パスワードの入力があった場合（入力エラー）
-if (empty($postName) && !empty($postPass)) {
-    $errors[] = '※ユーザー名が入力されていません。';
-}
-
-// ユーザー名の入力があり、パスワードに入力がなかった場合（入力エラー）
-if (!empty($postName) && empty($postPass)) {
-    $errors[] = '※パスワードが入力されていません。';
-}
-
-// ユーザー名とパスワードの両方が入力がされている場合（文字数エラー）
-if (!empty($postPass) && !empty($postPass)) {
+// ユーザー名のエラー処理
+if (isset($postName)) {
     if ($nameLength < $nameMinimum || $nameLength > $postMaximum) {
         $errors[] = '※ユーザー名が4～16文字ではありません。';
     }
+    if (preg_match('/[^a-zA-Z0-9_]/', $postName)) {
+        $errors[] = '※ユーザー名に入力が不可能な文字が含まれています。<br>
+                    （入力可能な文字は<a href="account_reg_str.php" target="_blank" rel="noopener noreferrer">こちら</a>を参考にしてください。）';
+    }
+    if (preg_match('/^[_]/', $postName)) {
+        $errors[] = '※ユーザー名の先頭に _（アンダーバー）は使用できません。';
+    }
+} else {
+    $errors[] = '※ユーザー名を入力してください（半角英数字とアンダーバー 4～16文字以内）';
+}
+
+// パスワードのエラー処理
+if (isset($postPass)) {
     if ($passLength < $passMinimum || $passLength > $postMaximum) {
         $errors[] = '※パスワードが6～16文字ではありません。';
     }
-}
-
-// ユーザー名とパスワードの両方が入力がされている場合（入力不可文字エラー）
-if (!empty($postPass) && !empty($postPass)) {
-    if (preg_match('/[^a-zA-Z0-9_]/', $postName)) {
-        $errors[] = '※ユーザー名に入力が不可能な文字が含まれています。<br>
-                    （入力可能な文字は<a href="account_reg_str.php">こちら</a>を参考にしてください。）';
-    };
     if (preg_match('/[^!-~]/', $postPass)) {
         $errors[] = '※パスワードに入力が不可能な文字が含まれています。<br>
-                    （入力可能な文字は<a href="account_reg_str.php">こちら</a>を参考にしてください。）';
-    }
-}
-
-// ユーザー名とパスワードの両方が入力がされている場合（先頭入力不可文字エラー）
-if (!empty($postPass) && !empty($postPass)) {
-    if (preg_match('/^[_]/', $postName)) {
-        $errors[] = '※ユーザー名の先頭に _（アンダーバー）は使用できません。<br>';
+                    （入力可能な文字は<a href="account_reg_str.php" target="_blank" rel="noopener noreferrer">こちら</a>を参考にしてください。）';
     }
     if (preg_match('/^[!-\/:-@[-`{-~]/', $postPass)) {
-        $errors[] = '※パスワードの先頭に 記号 は使用できません。<br>';
+        $errors[] = '※パスワードの先頭に記号は使用できません。';
     }
+} else {
+    $errors[] = '※パスワードを入力してください（半角英数字と記号 6～16文字以内）';
 }
 
+// エラーがない場合は確認画面に遷移
 if (!isset($errors)) {
     $_SESSION['name'] = $postName;
     $_SESSION['pass'] = $postPass;
     header('location: account_reg_cfm.php');
     exit();
 }
-
-
-
-
-// if (isset($_POST['name']) && isset($_POST['pass'])) {
-//     $_SESSION['name'] = $_POST['name'];
-//     $_SESSION['pass'] = $_POST['pass'];
-//     header('Location: account_reg_cfm.php');
-//     exit();
-// }
-
-
-// // $_POSTが空（' '空文字, 0, false, NULL）ではなければ実行
-//     if (!empty($_POST['name']) && !empty($_POST['pass'])) {
-//         $name = $_POST['name'];
-//         $pass = $_POST['pass'];
-//         $hash = password_hash($pass, PASSWORD_BCRYPT);
-//         $limit = 4;
-//         $passLength = strlen($pass);
-
-//         // $_POSTが空（' '空文字, 0, false, NULL）ではない & $_POST[password]が4文字以上か確認
-//         if ($passLength >= $limit) {
-//             $stmt = $dbh->prepare('INSERT INTO users (name, password) VALUES (:name, :password)');
-//             $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-//             $stmt->bindValue(':password', $hash, PDO::PARAM_STR);
-//             $stmt->execute();
-//             header ('Location: account_reg_cfm.php');
-//             exit();
-//         }
-
-//     }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -144,7 +105,7 @@ if (!isset($errors)) {
 
         <form action="" method="POST" class="account_reg">
             <h4><?php foreach ($errors as $error) {
-                    echo $error . '<br>'. '<br>';
+                    echo $error . '<br>' . '<br>';
                 } ?></h4>
             <p>ユーザー名　<input type="text" name="name" value="<?php if (isset($_POST['name'])) {
                                                                 htmlsc($_POST['name']);
@@ -156,7 +117,8 @@ if (!isset($errors)) {
         </form>
 
         <div class="account_reg_caution">
-            <p>※登録に使用できる文字は<a href="account_reg_str.php">こちら</a>を参考にしてください。</p>
+            <h4>【注意事項】</h4>
+            <p>※登録に使用できる文字は<a href="account_reg_str.php" target="_blank" rel="noopener noreferrer">こちら</a>を参考にしてください。</p>
             <p>※ユーザー名とパスワードは、同じものを使用しないでください。</p>
             <p>※ユーザー名とパスワードは、電話番号や誕生日などの個人情報を使用しないでください。</p>
             <p>※パスワードは、第三者に推測されやすいものを使用しないでください。（passwordやabcd1234など）</p>
