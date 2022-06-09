6378b2b5d25feb527ea8f1cba82dcb39068aadd6
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-06-09 20:06:56 +0900

    Add: メニューボタン

--- toppage.php
+++ toppage.php
@@ -15,6 +15,20 @@ require_once('fanctions.php');
 // echo var_dump($_SERVER['HTTP_HOST']);
 // echo var_dump($_SERVER['HTTP_REFERER']);
 // echo var_dump(strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']));
+
+if (isset($_SESSION['loginName'])) {
+    $display = '【ログイン中】ようこそ！' . $_SESSION['loginName'] . 'さん！';
+}
+
+if (isset($_SESSION['loginName']) && time() - $_SESSION['start'] > 15) {
+    unset($_SESSION['loginName'], $_SESSION['loginPass']);
+    $display = '時間が経過したため、ログイン状態が解除されました。<a href="login.php">再ログイン</a>';
+}
+$_SESSION['start'] = time();
+
+if (!isset($display)) {
+    $display = '※投稿したりスレッドを作成するには、<a href="account_reg.php">新規登録</a>または<a href="login.php">ログイン</a>が必要です。';
+}
 ?>
 
 
@@ -28,16 +42,40 @@ require_once('fanctions.php');
     <meta name="description" content="sample text">
     <link rel="stylesheet" type="text/css" href="board.css">
     <title>掲示板</title>
+    <script type="text/javascript">
+        window.addEventListener('DOMContentLoaded', () => {
+            const btn = document.querySelector('.menu_btn');
+            const nav = document.querySelector('nav');
+            btn.addEventListener('click', () => {
+                nav.classList.toggle('open-menu')
+                if (btn.innerHTML === 'Menu') {
+                  btn.innerHTML = 'Close';
+                } else {
+                  btn.innerHTML = 'Menu';
+                }
+            });
+        });
+    </script>
 </head>
 
 <body>
     <div class="header">
         <h1><a href="toppage.php">サンプル掲示板</a></h1>
+        <button class="menu_btn">Menu</button>
+        <nav class="menu_list">
+            <ul>
+                <li><a href="#">トップページ</a></li>
+                <li><a href="#">マイページ</a></li>
+                <li><a href="#">ログアウト</a></li>
+                <li><a href="#">お問い合わせ</a></li>
+                <li><a href="#">運営者情報</a></li>
+            </ul>
+        </nav>
     </div>
 
     <div class="main">
         <h2>この掲示板はポートフォリオ用となっています</h2>
-        <p>※投稿したりスレッドを作成するには、<a href="account_reg.php">新規登録</a>または<a href="login.php">ログイン</a>が必要です。</p>
+        <?php echo $display; ?>
     </div>
 
     <div class="main">
@@ -51,7 +89,7 @@ require_once('fanctions.php');
             <button class="thread_btn" type="button">スレッド1</button>
         </a>
     </div>
-    
+
     <div class="footer">
 
     </div>
