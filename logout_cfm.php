d35bda7b377f10871cbec97ed4ec3f2856ca32d5
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-06-09 22:39:44 +0900

    first commit

---
+++ logout_cfm.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="sample text">
+    <link rel="stylesheet" type="text/css" href="board.css">
+    <title>ログアウト確認</title>
+    <script type="text/javascript">
+        window.addEventListener('DOMContentLoaded', () => {
+            const btn = document.querySelector('.menu_btn');
+            const nav = document.querySelector('nav');
+            btn.addEventListener('click', () => {
+                nav.classList.toggle('open-menu')
+                if (btn.innerHTML === 'Menu') {
+                    btn.innerHTML = 'Close';
+                } else {
+                    btn.innerHTML = 'Menu';
+                }
+            });
+        });
+    </script>
+</head>
+
+<body>
+    <div class="header">
+        <h1><a href="toppage.php">サンプル掲示板</a></h1>
+        <button class="menu_btn">Menu</button>
+        <nav class="menu_list">
+            <ul>
+                <li><a href="toppage.php">トップページ</a></li>
+                <li><a href="mypage.php">マイページ</a></li>
+                <li><a href="logout_cfm.php">ログアウト</a></li>
+                <li><a href="inquiry.php">お問い合わせ</a></li>
+                <li><a href="admin.php">運営者情報</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="main">
+        <h2>ログアウト確認</h2>
+    </div>
+
+    <div class="footer">
+
+    </div>
+</body>
+
+</html>
