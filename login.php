6a93524f5179e50377ce376da47c3a10eb1e8b62
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-06-12 17:56:04 +0900

    Add: ログイン判定

--- login.php
+++ login.php
@@ -2,21 +2,25 @@
 session_start();
 require_once('db_board.php');
 require_once('fanctions.php');
-// echo var_dump($_);
-// echo var_dump($_POST);
-// echo var_dump($_SERVER);
-// echo var_dump($_SESSION);
-// echo var_dump($_COOKIE);
 
-// echo var_dump($_POST['name']);
-// echo var_dump($_POST['pass']);
-// echo var_dump($_SESSION['name']);
-// echo var_dump($_SESSION['pass']);
-// echo var_dump($_SERVER['HTTP_HOST']);
-// echo var_dump($_SERVER['HTTP_REFERER']);
-// echo var_dump(strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']));
+// ログイン判定
+if (isset($_SESSION['loginName'])) {
+    $loginJudge = 'ログイン中';
+} else {
+    $loginJudge = '未ログイン';
+}
 
+if (isset($_SESSION['loginName']) && time() - $_SESSION['start'] > 600) {
+    unset($_SESSION['loginName'], $_SESSION['loginPass']);
+    $display = '時間が経過したため、ログイン状態が解除されました。<a href="login.php">再ログイン</a>';
+}
+$_SESSION['start'] = time();
 
+if ($loginJudge == '未ログイン') {
+    $display = '※投稿したりスレッドを作成するには、<a href="account_reg.php">新規登録</a>または<a href="login.php">ログイン</a>が必要です。';
+}
+
+// 入力判定
 if (empty($_POST['name'])) {
     $errors[] = '※ユーザー名を入力してください';
 }
@@ -95,6 +99,7 @@ if (isset($passMatch)) {
     <div class="header">
         <h1 class="header_title"><a href="toppage.php">サンプル掲示板</a></h1>
         <button class="menu_btn">Menu</button>
+        <p><?php echo $loginJudge; ?></p>
         <nav class="menu_list">
             <ul>
                 <li><a href="toppage.php">トップページ</a></li>
@@ -123,13 +128,13 @@ if (isset($passMatch)) {
                 <p>パスワード　<input class="" type="text" name="pass" value="<?php if (isset($_POST['pass'])) {
                                                                             htmlsc($_POST['pass']);
                                                                         } ?>"></p>
-                <p><input class="btn login_btn" type="submit" name="login" value="ログイン"></p>
+                <p><input class="btn" type="submit" name="login" value="ログイン"></p>
             </form>
         </div>
 
         <p id="hazimete">はじめての方はこちら</p>
         <a href="account_reg.php">
-            <button class="btn new_account_btn" type="button">新規登録</button>
+            <button class="btn blue_btn new_account_btn" type="button">新規登録</button>
         </a>
 
         <div class="bottom">
--- toppage.php
+++ toppage.php
@@ -2,33 +2,24 @@
 session_start();
 require_once('db_board.php');
 require_once('fanctions.php');
-// echo var_dump($_);
-// echo var_dump($_POST);
-// echo var_dump($_SERVER);
-// echo var_dump($_SESSION);
-// echo var_dump($_COOKIE);
 
-// echo var_dump($_POST['name']);
-// echo var_dump($_POST['pass']);
-// echo var_dump($_SESSION['name']);
-// echo var_dump($_SESSION['pass']);
-// echo var_dump($_SERVER['HTTP_HOST']);
-// echo var_dump($_SERVER['HTTP_REFERER']);
-// echo var_dump(strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']));
-
+// ログイン判定
 if (isset($_SESSION['loginName'])) {
-    $display = '【ログイン中】ようこそ！' . $_SESSION['loginName'] . 'さん！';
+    $loginJudge = 'ログイン中';
+} else {
+    $loginJudge = '未ログイン';
 }
 
-if (isset($_SESSION['loginName']) && time() - $_SESSION['start'] > 15) {
+if (isset($_SESSION['loginName']) && time() - $_SESSION['start'] > 600) {
     unset($_SESSION['loginName'], $_SESSION['loginPass']);
     $display = '時間が経過したため、ログイン状態が解除されました。<a href="login.php">再ログイン</a>';
 }
 $_SESSION['start'] = time();
 
-if (!isset($display)) {
+if ($loginJudge == '未ログイン') {
     $display = '※投稿したりスレッドを作成するには、<a href="account_reg.php">新規登録</a>または<a href="login.php">ログイン</a>が必要です。';
 }
+
 ?>
 
 
@@ -62,6 +53,7 @@ if (!isset($display)) {
     <div class="header">
         <h1 class="header_title"><a href="toppage.php">サンプル掲示板</a></h1>
         <button class="menu_btn">Menu</button>
+        <p><?php echo $loginJudge; ?></p>
         <nav class="menu_list">
             <ul>
                 <li><a href="toppage.php">トップページ</a></li>
@@ -77,15 +69,17 @@ if (!isset($display)) {
 
         <div class="head">
             <h2>この掲示板はポートフォリオ用となっています</h2>
-            <?php echo $display; ?>
+            <?php if (isset($display)) {
+                echo $display;
+            } ?>
         </div>
-
+        
         <div class="content">
             <p><a href="new_thread.php">
-                    <button class="btn new_thread_btn" type="button">スレッド作成</button>
+                    <button class="btn blue_btn new_thread_btn" type="button">スレッド作成</button>
                 </a></p>
             <a href="thread1.php">
-                <button class="btn thread_btn" type="button">スレッド1</button>
+                <button class="btn blue_btn thread_btn" type="button">スレッド1</button>
             </a>
         </div>
 
