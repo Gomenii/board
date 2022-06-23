ca8082a5114dd841029f5cc17fc3850d4131c980
Matsumoto Ryuho <gomenii.work@gmail.com>
2022-06-23 17:47:35 +0900

    first commit

---
+++ res_delete.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once('db_board.php');
+require_once('fanctions.php');
+
+$resId = $_REQUEST['id'];
+echo $resId;
+$stmt = $dbh->prepare('DELETE FROM posts WHERE id = :id');
+$stmt->bindValue(':id', $resId, PDO::PARAM_INT);
+$stmt->execute();
+
+header('location:' . $_SERVER['HTTP_REFERER']);
+exit();
+?>
